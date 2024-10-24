b543836cb36c9bc2cc0b4f638b49008c4669997a
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2024-10-24 21:44:35 +0700

    update a navbar using react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import SignUp from './components/SignUp';
-import DisplayInfor from './components/DisplayInfor';
-import MyComponent from './components/MyComponent';
-
-const listUsers = [
-  {
-    id: '' + Math.floor(Math.random() * 10 + 2) + 2,
-    username: 'Quynh Anh',
-    email: 'huynhanh.170504@gmail.com'
-  },
-  {
-    id: '' + Math.floor(Math.random() * 10 + 2) + 2,
-    username: 'Arya',
-    email: 'arya@gmail.com'
-  },
-  {
-    id: '' + Math.floor(Math.random() * 10 + 2) + 2,
-    username: 'Hmm',
-    email: 'hmm@gmail.com'
-  },
-]
+import './App.scss'
+import Header from './components/Header/Header';
 
 const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
 
   return (
-    <div>
-      {/* <SignUp></SignUp> */}
-      <MyComponent></MyComponent>
+    <div className='app-container'>
+      <Header/>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
   );
 }
 
-export default App;
+export default App
