c7cfa427d7937a81f666aaf6c05f15c2469d8a48
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2024-10-27 00:07:19 +0700

    complete homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,6 +17,8 @@ const Header = () => {
             <NavLink to="/admin" className='nav-link'>Admin</NavLink>
           </Nav>
           <Nav>
+            <button className='btn-login'>Log in</button>
+            <button className='btn-signup'>Sign up</button>
             <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -3,14 +3,21 @@ import video_homepage from '../../assets/video-homepage.mp4'
 
 import './Homepage.scss'
 
-const Homepage = () => {
+const HomePage = () => {
   return (
     <div className='homepage-container'>
       <video autoPlay loop muted>
         <source src={video_homepage} type="video/mp4"/>
       </video>
+      <div className='homepage-content'>
+        <div className='para1'>There's a better way to ask</div>
+        <div className='para2'>You don't want to make a boring form. And your audience won't answer one. Create a typeform instead-and make everyone happy</div>
+        <div className='para3'>
+          <button>Get started, it's free</button>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Homepage
+export default HomePage
