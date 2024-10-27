e157d32de5dc024587dce4e9b9019810d66bc4f0
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2024-10-27 22:58:06 +0700

    add sidebar (uncompleted)

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { FaHeart, FaBars } from 'react-icons/fa'
 
-const Admin = (props) => {
+import SideBar from './SideBar.jsx'
+
+import './Admin.scss'
+
+const Admin = () => {
+  const [collapsed, setCollapsed] = useState(false)
+
   return (
-    <div>
-      Admin
-      <div>
-        <Link to="/">return to homepage</Link>
+    <div className='admin-container'>
+      <div className='admin-sidebar'>
+        <SideBar collapsed={collapsed}/>
+      </div>
+      <div className='admin-content'>
+        <FaBars onClick={() => {setCollapsed(!collapsed)}}/>
+        Admin
       </div>
     </div>
   )
